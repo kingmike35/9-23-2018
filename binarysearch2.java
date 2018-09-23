7ffc5e2f117add4599c5851e0c855ddde8c92c9b
kingmike35 <mgathers35@gmail.com>
2018-09-23 17:01:05 -0400

    Add files via upload

---
+++ binarysearch2.java
@@ -0,0 +1,10 @@
+package projecttree;
+
+public class binarysearch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ linearsearchalgorithm.java
@@ -0,0 +1,10 @@
+package projecttree;
+
+public class linearsearchalgorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
